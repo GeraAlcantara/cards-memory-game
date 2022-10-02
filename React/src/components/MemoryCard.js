31a6d8c121b28833edff3ab47e20bf9cc0913dce
Priyanshup891 <priyanshup891@gmail.com>
2022-10-02 11:31:12 +0530

    fix_the_react_refresh_bug

--- React/src/components/MemoryCard.js
+++ React/src/components/MemoryCard.js
@@ -1,11 +1,11 @@
 export default function MemoryCard(props) {
     return (
-        <div className="memory-card" data-framework={props.dataFramework} adss>
+        <div onClick={props.onClick} className="memory-card" data-framework={props.dataFramework}>
             <img className="front-face" src={props.frontFace} alt={props.alt} />
             <img
                 className="back-face"
                 // src={require("../img/js-badge.svg").default} Remove JS badge as back-face, may be confusing
-                src={require("../img/blank.svg").default}
+                src={require("../img/js-badge.svg").default}
                 alt={props.alt || props.dataFramework} // Alt will default to dataFramework, in baseGame every alt is the same as well
             />
         </div>
