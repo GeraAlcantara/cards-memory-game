918660ba61075a191b4ef413a45cc887d0f80081
Kusha-web <kushalmitra189@gmail.com>
2022-10-02 11:31:26 +0530

    Updated functionalites, Added Button to reinitiate the game and We can see number of turns played by the player

--- React/src/App.js
+++ React/src/App.js
@@ -1,130 +1,94 @@
 import MemoryCard from "./components/MemoryCard";
+import React, { useState, useEffect } from "react";
+
+const cardImages = [
+    { src: require("./img/angular.svg").default, matched: false },
+    { src: require("./img/react.svg").default, matched: false },
+    { src: require("./img/ember.svg").default, matched: false },
+    { src: require("./img/vue.svg").default, matched: false },
+    { src: require("./img/node.svg").default, matched: false },
+    { src: require("./img/svelte.svg").default, matched: false },
+];
 
 function App() {
-    const cards = document.querySelectorAll(".memory-card");
+    //const cards = document.querySelectorAll(".memory-card");
+    const [cards, setCards] = useState([]);
+    const [turns, setTurns] = useState(0);
+    const [choiceOne, setChoiceOne] = useState(null);
+    const [choiceTwo, setChoiceTwo] = useState(null);
+    const [disabled, setDisabled] = useState(false);
 
-    let hasFlippedCard = false;
-    let lockBoard = false;
-    let firstCard, secondCard;
+    // shuffle cards
+    const shuffleCards = () => {
+        const shuffledCards = [...cardImages, ...cardImages]
+            .sort(() => Math.random() - 0.5)
+            .map((card) => ({ ...card, id: Math.random() }));
 
-    function flipCard() {
-        if (lockBoard) {
-            return;
+        setCards(shuffledCards);
+        setChoiceOne(null);
+        setChoiceTwo(null);
+        setTurns(0);
+    };
+
+    // handle a choice
+    const handleChoice = (card) => {
+        choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+    };
+
+    //compare two cards
+    useEffect(() => {
+        if (choiceOne && choiceTwo) {
+            setDisabled(true);
+
+            if (choiceOne.src === choiceTwo.src) {
+                setCards((prevCards) => {
+                    return prevCards.map((card) => {
+                        if (card.src === choiceOne.src) {
+                            return { ...card, matched: true };
+                        } else {
+                            return card;
+                        }
+                    });
+                });
+                resetTurns();
+            } else {
+                setTimeout(() => resetTurns(), 1000);
+            }
         }
-        if (this === firstCard) {
-            return;
-        }
-        this.classList.add("flip");
+    }, [choiceOne, choiceTwo]);
 
-        if (!hasFlippedCard) {
-            hasFlippedCard = true;
-            firstCard = this;
-            return;
-        }
-        secondCard = this;
-        /* hasFlippedCard = false; */
 
-        checkForMatch();
-    }
+    // reset choices & increase turns
+    const resetTurns = () => {
+        setChoiceOne(null);
+        setChoiceTwo(null);
+        setTurns((prevTurns) => prevTurns + 1);
+        setDisabled(false);
+    };
 
-    function checkForMatch() {
-        let isMatch =
-            firstCard.dataset.framework === secondCard.dataset.framework;
-        isMatch ? disableCards() : unflipCards();
-    }
-
-    function disableCards() {
-        firstCard.removeEventListener("click", flipCard);
-        secondCard.removeEventListener("click", flipCard);
-        resetBoard();
-    }
-
-    function unflipCards() {
-        lockBoard = true;
-        setTimeout(() => {
-            firstCard.classList.remove("flip");
-            secondCard.classList.remove("flip");
-            resetBoard();
-        }, 1500);
-    }
-
-    function resetBoard() {
-        hasFlippedCard = lockBoard = false;
-        firstCard = secondCard = false;
-    }
-
-    cards.forEach((card) => {
-        const randomPos = Math.floor(Math.random() * cards.length);
-        card.style.order = randomPos;
-    });
-
-    cards.forEach((card) => card.addEventListener("click", flipCard));
+    //start game automatically
+    useEffect(() => {
+        shuffleCards();
+    }, []);
 
     return (
         <div className="App">
+            <button onClick={shuffleCards}>New Game</button>
             {/* <!-- Game container --> */}
             <section className="memory-game">
-                <MemoryCard
-                    dataFramework=""
-                    frontFace={require("./img/angular.svg").default}
-                    alt="angular"
-                />
-                <MemoryCard
-                    dataFramework=""
-                    frontFace={require("./img/angular.svg").default}
-                    alt="angular"
-                />
-                <MemoryCard
-                    dataFramework="React"
-                    frontFace={require("./img/react.svg").default}
-                    alt=""
-                />
-                <MemoryCard
-                    dataFramework="React"
-                    frontFace={require("./img/react.svg").default}
-                    alt=""
-                />
-                <MemoryCard
-                    dataFramework="ember"
-                    frontFace={require("./img/ember.svg").default}
-                    alt=""
-                />
-                <MemoryCard
-                    dataFramework="ember"
-                    frontFace={require("./img/ember.svg").default}
-                    alt=""
-                />
-                <MemoryCard
-                    dataFramework="vue"
-                    frontFace={require("./img/vue.svg").default}
-                    alt=""
-                />
-                <MemoryCard
-                    dataFramework="vue"
-                    frontFace={require("./img/vue.svg").default}
-                    alt=""
-                />
-                <MemoryCard
-                    dataFramework="node"
-                    frontFace={require("./img/node.svg").default}
-                    alt=""
-                />
-                <MemoryCard
-                    dataFramework="node"
-                    frontFace={require("./img/node.svg").default}
-                    alt=""
-                />
-                <MemoryCard
-                    dataFramework="svelte"
-                    frontFace={require("./img/svelte.svg").default}
-                    alt=""
-                />
-                <MemoryCard
-                    dataFramework="svelte"
-                    frontFace={require("./img/svelte.svg").default}
-                    alt=""
-                />
+                {
+                    cards.map((card) => (
+                        <MemoryCard
+                            key={card.id}
+                            card={card}
+                            handleChoice={handleChoice}
+                            flipped={card === choiceOne || card === choiceTwo || card.matched}
+                            disabled={disabled}
+                        />
+                    ))
+                }
             </section>
+            <p style={{fontSize: "1.4rem"}}>Turns : {turns}</p>
         </div>
     );
 }
--- React/src/components/MemoryCard.js
+++ React/src/components/MemoryCard.js
@@ -1,13 +1,22 @@
-export default function MemoryCard(props) {
+export default function MemoryCard({ card, handleChoice, flipped, disabled }) {
+    const handleClick = () => {
+        if (!disabled) {
+            handleChoice(card);
+        }
+    }
+
     return (
-        <div className="memory-card" data-framework={props.dataFramework} adss>
-            <img className="front-face" src={props.frontFace} alt={props.alt} />
-            <img
-                className="back-face"
-                // src={require("../img/js-badge.svg").default} Remove JS badge as back-face, may be confusing
-                src={require("../img/blank.svg").default}
-                alt={props.alt || props.dataFramework} // Alt will default to dataFramework, in baseGame every alt is the same as well
-            />
+        <div className="memory-card">
+            <div className={flipped ? "flipped" : ""}>
+                <img className="front-face" src={card.src} alt="" />
+                <img
+                    className="back-face"
+                    // src={require("../img/js-badge.svg").default} Remove JS badge as back-face, may be confusing
+                    src={require("../img/blank.svg").default}
+                    onClick={handleClick}
+                // alt={props.alt || props.dataFramework} // Alt will default to dataFramework, in baseGame every alt is the same as well
+                />
+            </div>
         </div>
     );
 }
