eb1371f1b8a61fd64e4b6cc55cd798b0db725aec
Amit Kumar Sharma <91947037+amit-2k1@users.noreply.github.com>
2022-10-09 11:34:29 +0530

    added memory card

--- Svelte/src/App.svelte
+++ Svelte/src/App.svelte
@@ -1,7 +1,41 @@
 <script>
+  import MemoryCard from "./components/MemoryCard.svelte";
 
+  const cards = [
+				{
+					name: "react",
+					face: "react.svg",
+				},
+				{
+					name: "ember",
+					face: "ember.svg",
+				},
+				{
+					name: "vue",
+					face: "vue.svg",
+				},
+				{
+					name: "node",
+					face: "node.svg",
+				},
+				{
+					name: "svelte",
+					face: "svelte.svg",
+				},
+				{
+					name: "angular",
+					face: "angular.svg",
+				},
+			]
 </script>
 
-<main>
-  <h1>Svelte Card Game</h1>
+<main class="memory-game">
+  {#each cards as card}
+    <MemoryCard 
+      card={card} 
+    />
+    <MemoryCard 
+      card={card} 
+    />
+  {/each}
 </main>
---
+++ Svelte/src/components/MemoryCard.svelte
@@ -0,0 +1,36 @@
+<script>
+  import backSVG from '../assets/js-badge.svg'
+  import frontSVG from '../assets/vue.svg'
+
+  // PROPS
+  export let card;
+
+  // STATE
+  let flipped = false;
+
+  // HANDLERS
+  function flip(e) {
+    flipped = !flipped
+    e.currentTarget.classList.toggle('flip')
+  }
+  
+</script>
+
+<div
+  class={`memory-card`}
+  on:click={flip}
+>
+  {#if !flipped}
+    <img
+      class={'back-face'}
+      src={backSVG}
+      alt="Click me!"
+    />
+  {:else}
+    <img
+      class='front-face'
+      src={`/src/assets/${card.face}`}
+      alt='card face'
+    />
+  {/if}
+</div>
